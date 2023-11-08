f9cbedcddac0bb5165ab0bbfc61c82e925e8e5ba
vgtoui <toni.uimonen@tuni.fi>
2023-11-08 16:55:03 +0200

    Add payment functionality to the BookService

--- backend/src/main/java/com/tau/springbootlibrary/service/BookService.java
+++ backend/src/main/java/com/tau/springbootlibrary/service/BookService.java
@@ -2,8 +2,10 @@ package com.tau.springbootlibrary.service;
 
 import com.tau.springbootlibrary.dao.BookRepository;
 import com.tau.springbootlibrary.dao.CheckoutRepository;
+import com.tau.springbootlibrary.dao.PaymentRepository;
 import com.tau.springbootlibrary.entity.Book;
 import com.tau.springbootlibrary.entity.Checkout;
+import com.tau.springbootlibrary.entity.Payment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
@@ -27,11 +29,14 @@ public class BookService {
 
     private HistoryRepository historyRepository;
 
+    private PaymentRepository paymentRepository;
+
     public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,
-                       HistoryRepository historyRepository) {
+                       HistoryRepository historyRepository, PaymentRepository paymentRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
         this.historyRepository = historyRepository;
+        this.paymentRepository = paymentRepository;
     }
 
     public Book checkoutBook (String userEmail, Long bookId) throws Exception {
@@ -42,6 +47,40 @@ public class BookService {
 
         if (!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
             throw new Exception("Book doesn't exist or already checked out by user");
+        }
+
+        List<Checkout> currentBooksCheckedOut = checkoutRepository.findBooksByUserEmail(userEmail);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd");
+
+        boolean bookNeedsReturned = false;
+
+        for (Checkout checkout : currentBooksCheckedOut) {
+            Date d1 = sdf.parse(checkout.getReturnDate());
+            Date d2 = sdf.parse((LocalDate.now().toString()));
+            TimeUnit time = TimeUnit.DAYS;
+
+            double differenceInTime = time.convert(
+                    d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+
+            if (differenceInTime < 0) {
+                bookNeedsReturned = true;
+                break;
+            }
+        }
+
+        Payment userPayment = paymentRepository.findByUserEmail(userEmail);
+
+        if ((userPayment != null && userPayment.getAmount() > 0) ||
+                (userPayment != null && bookNeedsReturned)) {
+            throw new Exception("Outstanding fees");
+        }
+
+        if (userPayment == null) {
+            Payment payment = new Payment();
+            payment.setAmount(00.00);
+            payment.setUserEmail(userEmail);
+            paymentRepository.save(payment);
         }
 
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
@@ -101,7 +140,7 @@ public class BookService {
 
                 TimeUnit time = TimeUnit.DAYS;
 
-                long difference_In_Time = time.convert(
+                double difference_In_Time = time.convert(
                         d1.getTime() - d2.getTime(),
                         TimeUnit.MILLISECONDS);
 
@@ -125,6 +164,24 @@ public class BookService {
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
 
         bookRepository.save(book.get());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdf.parse(validateCheckout.getReturnDate());
+        Date d2 = sdf.parse(LocalDate.now().toString());
+
+        TimeUnit time = TimeUnit.DAYS;
+
+        double differenceInTime = time.convert(
+                d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+
+        if (differenceInTime < 0) {
+            Payment payment = paymentRepository.findByUserEmail(userEmail);
+
+            payment.setAmount(payment.getAmount() + (differenceInTime * -1));
+            paymentRepository.save(payment);
+        }
+
         checkoutRepository.deleteById(validateCheckout.getId());
 
         History history = new History(
