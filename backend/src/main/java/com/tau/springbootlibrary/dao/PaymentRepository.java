caa74beed5f48f75e970737c5e6c2f90abee9332
vgtoui <toni.uimonen@tuni.fi>
2023-11-08 13:51:00 +0200

    Create entity, response model and service for payment

---
+++ backend/src/main/java/com/tau/springbootlibrary/dao/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.tau.springbootlibrary.dao;
+
+import com.tau.springbootlibrary.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Payment findByUserEmail(String userEmail);
+}
---
+++ backend/src/main/java/com/tau/springbootlibrary/service/PaymentService.java
@@ -0,0 +1,43 @@
+package com.tau.springbootlibrary.service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.tau.springbootlibrary.dao.PaymentRepository;
+import com.tau.springbootlibrary.requestmodels.PaymentInfoRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository,
+                          @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+
+    public PaymentIntent createPaymentIntent(
+            PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("currency", paymentInfoRequest.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
+    }
+}
